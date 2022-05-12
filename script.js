7554ca964cbd557a4b80e2ef456464b3c8d0cfc6
VetallS <fackq325@gmail.com>
2022-05-12 06:33:12 +0300

    A big commit
    1) Added: 
     - main content;
     - secondary content;
     - a footer.
    
    2) A lot of fixes of adaptive layout.
    3) Some fixes for different elements.

---
+++ script.js
@@ -0,0 +1,25 @@
+if (window.jQuery) {
+    console.log ('Jquery is connected');
+  } else {
+    console.log ('Jquery is not connected');
+  }
+
+/*
+  $(document).bind( 'mousewheel', function (e) { 
+    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    $(document.body).stop().animate( { 
+         scrollTop : nt 
+     } , 500 , 'easeInOutCubic' );  
+} );
+*/
+
+$(function(){
+
+  $('.button-2').on('click', function(e){
+    $('html,body').stop().animate({ scrollTop: $('#yakor').offset().top }, 1000);
+    e.preventDefault();
+  });
+  
+  });
