e7adbc86b2ef78eaf26acff43b44e227c0a9fc8e
VetallS <fackq325@gmail.com>
2022-05-14 04:58:59 +0300

    Added hover and click selectors for all buttons.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ if (window.jQuery) {
     console.log ('Jquery is connected');
   } else {
     console.log ('Jquery is not connected');
-  }
+}
 
-AOS.init();
+//AOS.init();
 
 /*
   $(document).bind( 'mousewheel', function (e) { 
@@ -17,6 +17,7 @@ AOS.init();
 } );
 */
 
+/*
 $(function(){
 
   $('.button-2').on('click', function(e){
@@ -24,4 +25,70 @@ $(function(){
     e.preventDefault();
   });
   
-  });
+});*/
+
+//Первая версия. Работает циклично на первом найденом элементе
+/*
+let buttonHeader = document.querySelector('.title-header');
+let aButtonHeader = document.querySelector('.active-header');
+buttonHeader.onclick = function () {
+  buttonHeader.classList.replace('title-header', 'active-header');
+  aButtonHeader.classList.replace('active-header', 'title-header');
+  buttonHeader = document.querySelector('.title-header');
+  aButtonHeader = document.querySelector('.active-header');
+}
+*/
+
+//Вторая версия. Запускается один раз
+/*function abc() {
+  document.querySelector('.title-header').classList.replace('title-header', 'active-header');
+  document.querySelector('.active-header').classList.replace('active-header', 'title-header');
+  document.querySelector('.title-header').onclick = abc;
+}
+document.querySelector('.title-header').onclick = abc;
+*/
+
+//Третья версия. Вообще странная
+/*
+var f;
+f = jQuery('.title-header').clone();
+
+function abc() {
+  f = jQuery('.title-header');
+  jQuery('.active-header').attr('class', 'title-header');
+  f.attr('class', 'active-header');
+  
+}
+jQuery('.title-header').on('click', abc);
+*/
+
+//Четвертая версия. Все равно значения переменных не меняются
+/*
+var a;
+var b;
+var c;
+
+a = jQuery('.title-header');
+b = jQuery('.active-header');
+c = jQuery('.sitle-header');
+
+a.on('click', function(){
+  a = jQuery('.title-header');
+  b = jQuery('.active-header');
+  a.attr('class', 'active-header');
+  b.attr('class', 'title-header');
+  var temp = a;
+  a = b;
+  b = temp;
+});
+
+c.on('click', function(){
+  c = jQuery('.sitle-header');
+  b = jQuery('.active-header');
+  c.attr('class', 'active-header');
+  b.attr('class', 'sitle-header');
+  var temp = c;
+  c = b;
+  b = temp;
+});
+*/
